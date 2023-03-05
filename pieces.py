a59e3c483c377b3f9ba29591a46cce209d8c8290
Neetrom <121823524+Neetrom@users.noreply.github.com>
2023-03-05 13:13:44 +0100

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -1,4 +1,11 @@
 import pygame
 
-class Piece(pygame.Surface):
-    def __init__ 
+class Piece(pygame.sprite.Sprite):
+    def __init__(self, piece_type, pos):
+        super().__init__()
+        self.image = pygame.image.load(f"graphics/{piece_type}.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+
+        self.color = piece_type[0]
+        self.points = 0
+        
