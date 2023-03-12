aa7c15978e1a9ae6bbf2cda3e4309c0ea5400fd4
Neetrom <121823524+Neetrom@users.noreply.github.com>
2023-03-12 22:30:34 +0100

    e

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from pieces import Piece, Rook
 
 class Game:
     def __init__(self):
@@ -8,16 +9,30 @@ class Game:
         pygame.display.set_caption("lmao")
         self.board = pygame.surface.Surface(BOARD_SIZE)
         self.generate_board()
+        self.logic_board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0]]
+                
 
-        self.pieces = [[0, 0, 0, 0, 0, 0, 0, 0],
+        self.pieces = [["BR", "BH", "BB", "BQ", "BK", "BB", "BH", "BR"],
+                       ["BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP"],
                        [0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0, 0, 0, 0]]
+                       [0, "WP", "WP", "WP", "WP", "WP", "WP", "WP"],
+                       ["WR", "WH", "WB", "WQ", "WK", "WB", "WH", "WR"]]
+        
+        self.piece_group = pygame.sprite.Group()
         self.import_pieces()
+        self.x = 0
+        self.y = 0
+        self.copy = 0
 
 
     def generate_board(self):
@@ -36,24 +51,54 @@ class Game:
         for row_index, row in enumerate(PIECES):
             for col_index, val in enumerate(row):
                 if val:
-                    self.pieces[row_index][col_index] = pygame.image.load(f"graphics/{val}.png").convert_alpha()
-
-    def draw_pieces(self):
-        for row_index, row in enumerate(self.pieces):
-            for col_index, val in enumerate(row):
-                if val:
-                    self.screen.blit(val, (col_index*TILE_SIZE, row_index*TILE_SIZE))
+                    temp_piece = Piece(val, (col_index*TILE_SIZE, row_index*TILE_SIZE))
+                    self.piece_group.add(temp_piece)
+                    if val[1] == "R":
+                        self.logic_board[row_index][col_index] = Rook(val[0], (row_index, col_index))
     
-    def mouse_move(self, event):
-        if event.type == pygame.mo
+    def mouse_move(self):
+        x, y = pygame.mouse.get_pos()
+        x = int((x-x%TILE_SIZE)/TILE_SIZE)
+        y = int((y-y%TILE_SIZE)/TILE_SIZE)
+        if self.copy != 0:
+            copy_rect = self.copy.get_rect(center = pygame.mouse.get_pos())
+            self.screen.blit(self.copy, copy_rect)
+        elif self.pieces[y][x] != 0:
+            self.x = y
+            self.y = x
+            self.copy = pygame.image.load(f"graphics/{self.pieces[y][x].type}.png").convert_alpha()
+            pygame.Surface.set_alpha(self.copy, 180)
+            copy_rect = self.copy.get_rect(center = pygame.mouse.get_pos())
+            self.screen.blit(self.copy, copy_rect)
+            
+    def check_valid_move(self):
+        dest_x, dest_y = pygame.mouse.get_pos()
+        dest_x = int((dest_x-dest_x%TILE_SIZE)/TILE_SIZE)
+        dest_y = int((dest_y-dest_y%TILE_SIZE)/TILE_SIZE)
+
+        temp_board = self.logic_board[self.x][self.y].all_available(self.pieces)
+        if temp_board[dest_y][dest_x] == "X":
+            self.logic_board[dest_y][dest_x] = self.logic_board[self.x][self.y]
+            self.logic_board[self.x][self.y] = 0
+ 
+            self.pieces[dest_y][dest_x] = self.pieces[self.x][self.y]
+            self.pieces[dest_y][dest_x].update_pos((dest_x*TILE_SIZE, dest_y*TILE_SIZE))
+            self.pieces[self.x][self.y] = 0
 
     def run(self):
         self.screen.blit(self.board, (0,0))
-        self.draw_pieces()
+        self.piece_group.draw(self.screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        if pygame.mouse.get_pressed()[0]:
+            self.mouse_move()
+        else:
+            if self.copy != 0:
+                self.check_valid_move()
+                self.copy = 0
+
         pygame.display.update()
 
 if __name__ == "__main__":
--- pieces.py
+++ pieces.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import DIRECTIONS
 
 class Piece(pygame.sprite.Sprite):
     def __init__(self, piece_type, pos):
@@ -6,6 +7,38 @@ class Piece(pygame.sprite.Sprite):
         self.image = pygame.image.load(f"graphics/{piece_type}.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
 
-        self.color = piece_type[0]
+        self.type = piece_type
         self.points = 0
-        
+    def update_pos(self,pos):
+        self.pos = pos
+        self.rect = self.image.get_rect(topleft=pos)
+
+class Piece_Not_Sprite:
+    def __init__(self, color, pos):
+        self.color = color
+        self.pos = pos
+
+    def move(self):
+        pass
+
+class Rook(Piece_Not_Sprite):
+    def all_available(self, b):
+        board = b.copy()
+        for direction in DIRECTIONS:
+            check = [self.pos[0], self.pos[1]]
+            attack = False
+            while True:
+                check[0] += direction[0]
+                check[1] += direction[1]
+                if check[0] == 8 or check[0] == -1 or check[1] == 8 or check[1] == -1:
+                    break
+                attacking = board[check[0]][check[1]]
+                if attacking != 0 and attacking != "X":
+                    if attacking.type[0] == self.color:
+                        break
+                    elif attack:
+                        break
+                    else:
+                        attack = True
+                board[check[0]][check[1]] = "X"
+        return board
